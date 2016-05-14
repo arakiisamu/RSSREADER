e98e5820b2eb05f15b4980a5f66e417b5462630a
araki isamu <isamu@araki-no-macbook-pro.local>
2016-05-14 20:37:00 +0900

    Initial Commit

---
+++ RSSREADER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSSREADER
+//
+//  Created by araki isamu on 2016/05/14.
+//  Copyright © 2016年 araki isamu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
