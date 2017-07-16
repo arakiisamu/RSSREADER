8164a51f9c891031c2c49e572b995f3040cb949c
araki isamu <arakiisamu@arakiisamu-no-macbook-air.local>
2017-07-16 13:53:35 +0900

    Swift3対応

--- RSSREADER/ViewController.swift
+++ RSSREADER/ViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSXMLParserDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, XMLParserDelegate {
 
-    let feedUrl : NSURL? = NSURL(string: "http://b.hatena.ne.jp/hotentry/it.rss")
+    let feedUrl : URL? = URL(string: "http://b.hatena.ne.jp/hotentry/it.rss")
     
     @IBOutlet var tableView: UITableView!
     
@@ -21,7 +21,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Do any additional setup after loading the view, typically from a nib.
         
         if let unfeedUrl = feedUrl {
-            let parser : NSXMLParser? = NSXMLParser(contentsOfURL: unfeedUrl)
+            let parser : XMLParser? = XMLParser(contentsOf: unfeedUrl)
             if let unparser = parser {
                 unparser.delegate = self;
                 unparser.parse()
@@ -35,21 +35,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     // セルの行数
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
     
     // セルの内容を変更
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "Cell")
         let item = items[indexPath.row]
         cell.textLabel?.text = item.title
         return cell
     }
     
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let item = items[indexPath.row]
-        UIApplication.sharedApplication().openURL(NSURL(string: item.url!)!)
+        UIApplication.shared.open(URL(string: item.url!)!, options: [:], completionHandler: nil)
     }
     
     
@@ -59,10 +59,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     let titleElementName = "title"
     let linkElementName   = "link"
     
-    func parserDidStartDocument(parser: NSXMLParser) {
+    func parserDidStartDocument(_ parser: XMLParser) {
     }
     
-    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
         currentElementName = nil
         if elementName == itemElementName {
             items.append(Item())
@@ -73,7 +73,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     
     
-    func parser(parser: NSXMLParser, foundCharacters string: String) {
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
         if items.count > 0 {
             let lastItem = items[items.count-1]
             if let unCurrentElementName = currentElementName {
@@ -87,11 +87,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
-    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
         currentElementName = nil;
     }
     
-    func parserDidEndDocument(parser: NSXMLParser) {
+    func parserDidEndDocument(_ parser: XMLParser) {
         self.tableView.reloadData()
     }
 
